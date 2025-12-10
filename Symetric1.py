9312ba9f322e9f1dadfa059c758f74f39779b6cf
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 13:51:30 +0530

    It make set permanently after change

---
+++ Symetric1.py
@@ -0,0 +1,5 @@
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+set1.symmetric_difference_update(set2) # It will Make change in existing set if i will use ^ it will make new set
+print(set1)
