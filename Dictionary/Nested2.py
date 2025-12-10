062d89ab36b380d60894bd61adce0d6ed5adda92
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 17:02:49 +0530

    Multiple Dict in one dict

---
+++ Dictionary/Nested2.py
@@ -0,0 +1,6 @@
+data = { "class": {"student": {"name": "Mike","marks": {    "physics": 70,    "history": 80}}}}
+
+
+#Multiple Nested Dictionary
+dict1=data["class"]["student"]["marks"]["history"]
+print("History Marks is :",dict1)
