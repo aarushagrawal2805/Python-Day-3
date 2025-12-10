6a887080388e193245872207ffa72b488419c207
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 18:10:00 +0530

    Nested dict

--- Dictionary/Nested.py
+++ Dictionary/Nested.py
@@ -1,5 +1,7 @@
 data = {'person': {'name': 'Amit', 'age': 30}}
 
+def get_age(info):
+    return info["person"]["age"]
 #Nested Dictionary
-dict1=data["person"]["age"]
+dict1=get_age(data)
 print("Amit Age is :",dict1)
