3590ec219541c826c574d5f9fc13eb843a99f4a9
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-10 13:40:28 +0530

    Subset Check

---
+++ Subsetcheck.py
@@ -0,0 +1,7 @@
+#Check if set1 is a subset of set2. Write a code to return True if every element in the subset_set is also present in the main_set.
+
+Set1={20,30}
+Set2={20,30,40}
+
+Set3=Set1.issubset(Set2)
+print("If both are same print true :",Set3)
